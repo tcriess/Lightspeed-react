320d70da1c1eb68ea5d05861e9241ec6bccd35c6
Thorsten Riess <tr@scientific-wave.com>
2021-03-23 10:37:51 +0100

    Keycloak login working

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -6,7 +6,6 @@ import {
   ChatBody, ChatMessages, ChatFooter, LoginLogout,
 } from "../styles/liveChatStyles";
 import ReconnectingWebSocket from "reconnecting-websocket";
-import GoogleLogin, {GoogleLogout} from "react-google-login";
 import {useImmer} from "use-immer";
 import Moment from "react-moment";
 import 'moment/locale/de';
@@ -15,21 +14,22 @@ import {emojify} from "react-emojione";
 import he from 'he';
 import {useKeycloak} from "@react-keycloak/web";
 
-const URL = `${process.env.REACT_APP_WS_URL}`;
-const CLIENTID = `${process.env.REACT_APP_CLIENT_ID}`;
+const URL = `${process.env.REACT_APP_WS_URL}` || (window.location.protocol === 'https:' ? "wss://" + window.location.host + "/chat/default" : "ws://" + window.location.host + "/chat/default");
 
 const LiveChat = () => {
-  const [url, setUrl] = useState(URL + "?language=" + (navigator.language?.toLowerCase() || 'en'));
+  const [url, setUrl] = useState(URL + "?language=" + (navigator.language?.toLowerCase().slice(0, 2) || 'en'));
   const [messages, setMessages] = useImmer({list: [], obj: {}});
   const [input, setInput] = useState('');
   const [translationsById, setTranslationsById] = useImmer({});
   const [idtoken, setIdtoken] = useState("");
   const messagesEndRef = useRef();
   const [ws, setWs] = useState(null);
-  const { keycloak, initialized } = useKeycloak()
+  const {keycloak, initialized} = useKeycloak()
 
-  useEffect( () => {
-    keycloak.onAuthSuccess = () => { setIdtoken(keycloak.idToken); };
+  useEffect(() => {
+    keycloak.onAuthSuccess = () => {
+      setIdtoken(keycloak.idToken);
+    };
   }, []);
 
   useEffect(() => {
@@ -59,7 +59,7 @@ const LiveChat = () => {
         case 'chat':
           console.log('received chat event:', message.data)
           const msgId = message.data.id;
-          const newMessage = [message.data.id, message.data.created, message.data.source.user.nick || message.data.source.plugin_name + ' (bot)', he.decode(message.data.tags.message), ""]
+          const newMessage = [message.data.id, message.data.created, message.data.source.user.nick || message.data.source.plugin_name + ' (bot)', message.data.tags.message, ""]
           setMessages(draft => {
             if(draft.obj[msgId] === undefined) {
               draft.obj[msgId] = newMessage;
@@ -72,8 +72,8 @@ const LiveChat = () => {
           setMessages(draft => {
             message.data.map((d) => {
               if(draft.obj[d.data.id] === undefined) {
-                draft.obj[d.data.id] = [d.data.id, d.data.created, d.data.source.user.nick || d.data.source.plugin_name + ' (bot)', he.decode(d.data.tags.message), ""];
-                draft.list = [...draft.list, [d.data.id, d.data.created, d.data.source.user.nick || d.data.source.plugin_name + ' (bot)', he.decode(d.data.tags.message), ""]];
+                draft.obj[d.data.id] = [d.data.id, d.data.created, d.data.source.user.nick || d.data.source.plugin_name + ' (bot)', d.data.tags.message, ""];
+                draft.list = [...draft.list, [d.data.id, d.data.created, d.data.source.user.nick || d.data.source.plugin_name + ' (bot)', d.data.tags.message, ""]];
               }
             });
             draft.list = draft.list.sort((a, b) => a[1] > b[1] ? 1 : -1);
@@ -83,14 +83,14 @@ const LiveChat = () => {
           console.log('received translation event:', message.data)
           // the server is aware which language the client wants, no need to filter it here then
           const newTranslation = [message.data.tags.source_id, message.data.tags.message];
-          setTranslationsById(draft => ({...draft, [newTranslation[0]]: "(" + he.decode(newTranslation[1]) + ")"}));
+          setTranslationsById(draft => ({...draft, [newTranslation[0]]: "(" + newTranslation[1] + ")"}));
           break;
         case 'translations':
           console.log('received translations event:', message.data)
           // the server is aware which language the client wants, no need to filter it here then
           setTranslationsById(draft => {
             message.data.map((d) => {
-              draft[d.data.tags.source_id] = '(' + he.decode(d.data.tags.message) + ')';
+              draft[d.data.tags.source_id] = '(' + d.data.tags.message + ')';
             });
           })
           break;
@@ -107,13 +107,12 @@ const LiveChat = () => {
     scrollToBottom()
   }, [messages.list, translationsById]);
 
-  //const socketRef = useRef();
-  const [locale, setLocale] = useState(navigator.language || 'en');
+  const [locale, setLocale] = useState(navigator.language.toLowerCase().slice(0, 2) || 'en');
 
   useEffect(() => {
-    let lang = locale[0] + locale[1];
+    let lang = locale.toLowerCase().slice(0, 2);
     if(lang.match(/en|de|es/i)) {
-      setLocale(lang.toLowerCase());
+      setLocale(lang);
     } else {
       setLocale('en')
     }
@@ -130,20 +129,6 @@ const LiveChat = () => {
       setInput('');
     }
   };
-
-  const responseGoogle = (response) => {
-    console.log("login response:", response);
-    if(response.tokenId !== undefined) {
-      setIdtoken(response.tokenId);
-    } else {
-      setIdtoken("");
-    }
-  }
-
-  const logoutGoogle = (response) => {
-    console.log("logout: ", response);
-    setIdtoken("");
-  }
 
   const handleLocaleChange = (event) => {
     setLocale(event.target.value);
@@ -165,30 +150,10 @@ const LiveChat = () => {
     setUrl(u);
   }, [locale, idtoken]);
 
-  /*
-  <GoogleLogin
-              disabled={idtoken !== ''}
-              clientId={CLIENTID}
-              buttonText="Login"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy={'single_host_origin'}
-              isSignedIn={true}
-            />
-            <div style={{width: '10px'}}/>
-            <GoogleLogout
-              disabled={idtoken === ''}
-              clientId={CLIENTID}
-              buttonText="Logout"
-              onLogoutSuccess={logoutGoogle}
-            >
-            </GoogleLogout>
-   */
-
   const handleLogin = () => {
-    keycloak.login();
+    keycloak.login({scope: "email profile roles offline_access"});
     console.log("login called.")
-    if (keycloak.authenticated) {
+    if(keycloak.authenticated) {
       console.log("authenticated, token:", keycloak.idToken)
       setIdtoken(keycloak.idToken);
     } else {
@@ -226,8 +191,9 @@ const LiveChat = () => {
                                                                                                         fromNowDuring={20 * 60000}
                                                                                                         locale={locale}
                                                                                                         local>{m[1]}</Moment>)</small>
-                </p> <p className="innermsg" style={{textAlign: 'left'}}>{m[3]} <em
-                  style={{fontStyle: 'italic'}}>{translationsById[m[0]]}</em></p>
+                </p>{m[3].split("\n").map((l,i) => (<p key={i} className="innermsg" style={{textAlign: 'left'}}>{l}</p>))}
+                <p>
+                  <em style={{fontStyle: 'italic'}}>{translationsById[m[0]]}</em></p><br/>
                   <div ref={messagesEndRef}/>
                 </div>))
             }
@@ -239,13 +205,13 @@ const LiveChat = () => {
             <input style={{width: '100%', textAlign: 'left', paddingLeft: '3px'}} onChange={handleInput} value={input}/>
             <button style={{width: '75px'}} type="submit">Send</button>
           </form>
+          {initialized &&
           <LoginLogout>
-            <div>{`User is ${
-              !keycloak.authenticated ? 'NOT ' : keycloak.idTokenParsed.email + ' '
-            }authenticated`}</div>
+            <div>{keycloak.authenticated && `Welcome, ${keycloak.idTokenParsed.preferred_username}!`}&nbsp;</div>
             {!keycloak.authenticated && <button onClick={handleLogin}>Login</button>}
-            {!!keycloak.authenticated && <button onClick={handleLogout}>Logout</button>}
+            {keycloak.authenticated && <button onClick={handleLogout}>Logout</button>}
           </LoginLogout>
+          }
         </ChatFooter>
       </ChatMain>
     </ChatContainer>
